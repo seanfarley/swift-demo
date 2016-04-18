7618a1edeb8d25fabb860d16e70c81a2c55e5c4a
Sean Farley <sean@farley.io>
2016-04-18 16:27:16 -0700

    Initial Commit

---
+++ demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  demo
+//
+//  Created by Sean Farley on 4/18/16.
+//  Copyright © 2016 Sean Farley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
